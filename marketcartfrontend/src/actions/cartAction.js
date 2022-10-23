8e8b2757ee736f1f66bf7eb5efcb61251450ffb7
Arjun Dilip Laxane <99319693+arjunlaxane@users.noreply.github.com>
2022-10-23 11:25:15 +0530

    localstorage commit

--- marketcartfrontend/src/actions/cartAction.js
+++ marketcartfrontend/src/actions/cartAction.js
@@ -32,7 +32,7 @@ export const removeItemsFromCart = id => async (dispatch, getState) => {
     type: REMOVE_CART_ITEM,
     payload: id,
   });
-  localStorage.removeItem('cartItems');
+  localStorage.removeItem('cartItems',JSON.stringify(getState().cart.cartItems));
 
   // localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
 };
